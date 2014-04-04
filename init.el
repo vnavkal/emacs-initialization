3a0e24de2346f21132ea568ec33d9d578d43f44f
Viraj <vnavkal0@gmail.com>
2014-04-04 16:35:20 -0700

    Magit and key to delete all dired buffers

--- init.el
+++ init.el
@@ -66,3 +66,19 @@
  'eshell-mode-hook
  (lambda ()
    (setq pcomplete-cycle-completions nil)))
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Kill all dired buffers
+(defun kill-all-dired-buffers ()
+  (interactive)
+  (save-excursion
+    (let ((count 0))
+      (dolist (buffer (buffer-list))
+	(set-buffer buffer)
+	(when (equal major-mode 'dired-mode)
+	  (setq count (1+ count))
+	  (kill-buffer buffer)))
+      (message "Killed %i dired buffer(s)." count))))
+(global-set-key (kbd "C-x D") 'kill-all-dired-buffers)
