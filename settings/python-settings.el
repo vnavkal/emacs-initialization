4eea50c904d66d883427a9bbbc0f9347c41deee8
Viraj Navkal <vnavkal0@gmail.com>
2016-01-03 14:33:50 -0500

    look for miniconda python

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -1,6 +1,7 @@
 (let ((python-binary
-      (cond ((file-exists-p "~/anaconda3/bin/ipython") "~/anaconda3/bin/ipython")
-	    ((file-exists-p "~/anaconda/bin/ipython") "~/anaconda/bin/ipython"))))
+       (cond ((file-exists-p "~/anaconda3/bin/ipython") "~/anaconda3/bin/ipython")
+	     ((file-exists-p "~/miniconda3/bin/ipython") "~/miniconda3/bin/ipython")
+	     ((file-exists-p "~/anaconda/bin/ipython") "~/anaconda/bin/ipython"))))
   (setq python-shell-interpreter python-binary))
 
 (add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 100)))
