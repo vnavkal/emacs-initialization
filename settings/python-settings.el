2b1990b564e167a668715e2305a8a7c6d5c5a7b7
Viraj Navkal <vnavkal0@gmail.com>
2016-08-28 15:23:19 -0700

    fix ipython

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -4,6 +4,8 @@
 	     ((file-exists-p "~/anaconda/bin/ipython") "~/anaconda/bin/ipython"))))
   (setq python-shell-interpreter python-binary))
 
+(setq python-shell-interpreter-args "--simple-prompt -i")
+
 (add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 100)))
 
 (provide 'python-settings)
