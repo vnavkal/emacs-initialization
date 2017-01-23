9fa85cd6b0620ce5fad4d76217d21a0315f03f6b
Viraj Navkal <vnavkal0@gmail.com>
2017-01-22 18:54:55 -0800

    use elpy

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -1,10 +1,16 @@
-(let ((python-binary
-       (cond ((file-exists-p "~/anaconda3/bin/ipython") "~/anaconda3/bin/ipython")
-	     ((file-exists-p "~/miniconda3/bin/ipython") "~/miniconda3/bin/ipython")
-	     ((file-exists-p "~/anaconda/bin/ipython") "~/anaconda/bin/ipython"))))
-  (setq python-shell-interpreter python-binary))
+(elpy-enable)
 
-(setq python-shell-interpreter-args "--simple-prompt -i")
+;; fix for "native-completion" error, see
+;; https://github.com/jorgenschaefer/elpy/issues/887
+;; this can be removed in emacs 25.2
+(defun python-shell-completion-native-try ()
+  "Return non-nil if can trigger native completion."
+  (let ((python-shell-completion-native-enable t)
+	   (python-shell-completion-native-output-timeout
+	    python-shell-completion-native-try-output-timeout))
+       (python-shell-completion-native-get-completions
+	(get-buffer-process (current-buffer))
+	nil "_")))
 
 (add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 100)))
 
