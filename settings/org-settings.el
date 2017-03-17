eb394738c4244346c760a8327f1e4ba9f253e78b
Viraj <vnavkal0@gmail.com>
2017-03-17 16:20:55 -0700

    adjust org-mode settings

--- settings/org-settings.el
+++ settings/org-settings.el
@@ -2,5 +2,7 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-clock-clocktable-default-properties '(:sort (1 . ?a)))
+(setq org-startup-truncated nil)
 
 (provide 'org-settings)
