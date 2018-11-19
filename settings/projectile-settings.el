071e295a6c542117529290992c33ed20ad6aa5be
Viraj Navkal <viraj@Virajs-MacBook-Pro.local>
2018-11-19 14:01:59 -0800

    projectile keybindings

--- settings/projectile-settings.el
+++ settings/projectile-settings.el
@@ -7,4 +7,8 @@
 
 (global-set-key (kbd "M-.") 'helm-etags-select)
 
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
 (provide 'projectile-settings)
