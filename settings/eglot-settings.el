4c43d02f31f65b8f9f6a99f91f7181761cd78b1a
Viraj Navkal <vnavkal0@gmail.com>
2024-01-20 15:28:30 -0800

    use eglot for python files

--- settings/eglot-settings.el
+++ settings/eglot-settings.el
@@ -1,4 +1,5 @@
 ;; See https://github.com/joaotavora/eglot/blob/28c1c3a52e1cb7fa7260815eb53700f348d48dd5/eglot.el#L402-L404
 (setq eglot-withhold-process-id "1")
+(add-hook 'python-mode-hook 'eglot-ensure)
 
 (provide 'eglot-settings)
