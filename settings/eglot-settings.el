f2d9b4c45dd1796ffac70a5c3e61a59ce34d07f9
Viraj Navkal <vnavkal0@gmail.com>
2024-04-01 18:02:08 -0700

    use pyright language server

--- settings/eglot-settings.el
+++ settings/eglot-settings.el
@@ -1,5 +1,12 @@
 ;; See https://github.com/joaotavora/eglot/blob/28c1c3a52e1cb7fa7260815eb53700f348d48dd5/eglot.el#L402-L404
 (setq eglot-withhold-process-id "1")
-(add-hook 'python-mode-hook 'eglot-ensure)
+
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs '(python-mode . ("npx" "pyright-langserver" "--stdio")))
+  :hook
+  ((python-mode . eglot-ensure))
+  :bind (("M-." . eglot-find-declaration)))
 
 (provide 'eglot-settings)
