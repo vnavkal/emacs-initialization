4424962cb4f79603cb60908eaed113d77d51593e
Viraj Navkal <vnavkal0@gmail.com>
2026-03-10 11:18:41 -0700

    try to improve magit performance over TRAMP

--- settings/magit-settings.el
+++ settings/magit-settings.el
@@ -8,6 +8,9 @@
 ;; don't confirm before pushing
 (setq magit-push-always-verify nil)
 
+;; fewer commits in status log = fewer tag-lookup SSH round-trips over TRAMP
+(setq magit-log-section-commit-count 5)
+
 (use-package forge
   :after magit)
 
